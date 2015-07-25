b957c9707071535b6e0b7a864b6bcca0db254cde
Andrew Clinton <ddrew.clinton@gmail.com>
2015-07-25 19:31:20 -0400

    you can add run-time code in here if needed

---
+++ app/app.run.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+    angular
+        .module('app.run', [])
+        .run(run);
+
+    run.$inject = ['$rootScope'];
+
+    function run($rootScope) {
+       
+    }
+})();
