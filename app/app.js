336d2cd5223d533047c974c1a110323e312d439d
Andrew Clinton <ddrew.clinton@gmail.com>
2015-08-26 21:50:00 -0400

    add module for services and factories

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 (function () {
 	'use strict';
 
-	angular.module('app', ['ui.router', 'app.run', 'app.config', 'app.controllers']);
+	angular.module('app', ['ui.router', 'app.run', 'app.config', 'app.controllers', 'app.factories', 'app.services']);
 })();
